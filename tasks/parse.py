cea19b5b1ca23eb8a80a6e7dfe7fa920c8649697
mjbommar <michael@bommaritollc.com>
2013-07-28 10:13:20 -0400

    Modifying tasks/parse to gracefully handle exceptions and only output exception-relevant information.

--- tasks/parse.py
+++ tasks/parse.py
@@ -1,11 +1,12 @@
 import os, json
 import utils
 import uscode
+import traceback
 
 def run(options):
   title_number = options.get('title', None)
   # section = options.get('section', None)
-  year = options.get('year', 2011) # default to 2011 for now
+  year = options.get('year', 2011)  # default to 2011 for now
 
   if not title_number:
     utils.log("Supply a 'title' argument to parse a title.")
@@ -22,20 +23,35 @@ def run(options):
   count = 0
   for section in sections:
     section_number = section.enum()
-    print "[%s USC %s] Parsing..." % (title_number, section_number)
-
+    section_name = section.name()
+    section_citation = u"usc/{}/{}".format(title_number, section_number)
+    
     # this could probably be black boxed a little further?
-    bb = section.body_lines()
-    xx = uscode.GPOLocatorParser(bb)
-    qq = xx.parse()
-    output = qq.json()
-
-    utils.write(
-      json.dumps(output, sort_keys=True, indent=2), 
-      uscode_output(year, title_number, section_number)
-    )
-
-    count += 1
+    try:
+        bb = section.body_lines()
+        xx = uscode.GPOLocatorParser(bb)
+        qq = xx.parse()
+    
+        section_inner_output = qq.json()
+        section_outer_output = {'title': title_number,
+                                'number': section_number,
+                                'citation': section_citation,
+                                'name': section_name,
+                                'data': section_inner_output                            
+                                }
+        output = section_outer_output
+    
+        utils.write(
+          json.dumps(output, sort_keys=True, indent=2),
+          uscode_output(year, title_number, section_number)
+        )
+    
+        count += 1
+    except Exception, E:
+        print("")
+        print(u"Parsing {}.".format(section_citation))        
+        print(traceback.format_exc(E))
+        continue
 
   print "\nParsed %s sections of title %s." % (count, title_number)
 
